6ad156eecc7108661a46129884d0bcf4054e804e
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-23 16:40:56 +0300

    Uploaded homepage controller

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    // Function to show 6 newest posts
+    public function newestPosts()
+    {
+        // Kodu raksti šeit
+    }
+
+    // Function to show 6 most popular posts (based on likes count)
+    public function popularPosts()
+    {
+        // Kodu raksti šeit
+    }
+
+    // Function to show 6 posts from users followed by the authenticated user
+    public function followingPosts()
+    {
+        // Kodu raksti šeit
+    }
+
+    // Function to show 6 trending posts (based on likes and date)
+    public function trendingPosts()
+    {
+       // Kodu raksti šeit
+    }
+}
