bb89da003fe00552cf05388b9c5b09bb8042a74a
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-23 16:21:02 +0300

    Add files via upload

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Follower;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    // Parāda lietotāja vārdu, profila bildi, reģistrācijas datumu
+    public function showInfo($userId)
+    {
+        // Kodu raksti šeit
+    }
+}
+
---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class SearchController extends Controller
+{
+    public function filter(Request $request)
+    {
+        // Kodu raksti šeit
+    }
+}
