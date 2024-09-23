f8b17ca30b98d80419aa9823716c5a8160f13f0d
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-23 16:21:42 +0300

    Add files via upload

---
+++ app/Http/Controllers/FollowerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Follower;
+use Illuminate\Support\Facades\Auth;
+
+class FollowerController extends Controller
+{
+    public function follow($followedUserId)
+    {
+        // Kodu raksti šeit
+    }
+
+    public function unfollow($followedUserId)
+    {
+        // Kodu raksti šeit
+    }
+
+
+}
+
