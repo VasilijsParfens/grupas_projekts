975a9dd4e97a5ac9cde2c38014d7d9e1cc52c4c7
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-24 15:02:33 +0300

    Made recent posts dynamic
    Implemented newest posts function on homepage

--- resources/views/posts/homepage.blade.php
+++ resources/views/posts/homepage.blade.php
@@ -120,31 +120,18 @@
     <div class="homepage-wide-container">
         <div class="homepage-block-title">Recent</div>
         <div class="homepage-content-slots">
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 1</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 2</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 3</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 4</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 5</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 6</div>
-            </div>
+            @if(count($newestPosts) > 0)
+                    @foreach($newestPosts as $post)
+                    <div class="homepage-slot">
+                        <div class="homepage-slot-image"><img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image"></div>
+                        <div class="homepage-slot-text">Slot 1</div>
+                    </div>
+                    @endforeach
+            @else
+                <p>No posts found</p>
+            @endif
         </div>
     </div>
 </div>
+
 @endsection
