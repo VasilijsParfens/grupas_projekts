33d92dd73e5636cadd1377c559008f42bf667d12
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-24 17:24:23 +0300

    Made following section unavailable for guests in homepage.blade.php

--- resources/views/posts/homepage.blade.php
+++ resources/views/posts/homepage.blade.php
@@ -59,6 +59,7 @@
             </div>
         </div>
     </div>
+    @auth
     <div class="homepage-wide-container">
         <div class="homepage-block-title">Following</div>
         <div class="homepage-content-slots">
@@ -88,35 +89,7 @@
             </div>
         </div>
     </div>
-    <div class="homepage-wide-container">
-        <div class="homepage-block-title">Recommended</div>
-        <div class="homepage-content-slots">
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 1</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 2</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 3</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 4</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 5</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 6</div>
-            </div>
-        </div>
-    </div>
+    @endauth
     <div class="homepage-wide-container">
         <div class="homepage-block-title">Recent</div>
         <div class="homepage-content-slots">
