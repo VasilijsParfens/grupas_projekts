98ba50451d9f0aba3010aa58a3ba18529af1f02d
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-09-25 18:37:19 +0300

    Update trending.blade.php
    Made dynamic

--- resources/views/posts/trending.blade.php
+++ resources/views/posts/trending.blade.php
@@ -4,63 +4,22 @@
     <div class="browse-tabs">
         <a href="/newest"><div class="browse-tab" id="newTab">New</div></a>
         <a href="/popular"><div class="browse-tab" id="popularTab">Popular</div></a>
+        <a href="/trending"><div class="browse-tab browse-active" id="trendingTab">Trending</div></a>
         @auth
             <a href="/following"><div class="browse-tab" id="followingTab">Following</div></a>
         @endauth
-        <a href="/trending"><div class="browse-tab browse-active" id="trendingTab">Trending</div></a>
     </div>
 
     <h2 style="text-align: center;">Trending posts</h2>
 
     <div class="browse-content-slots">
-        <!-- Post slots -->
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 1</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 2</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 3</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 4</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 5</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 6</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 7</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 8</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 9</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 10</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 11</div>
-        </div>
-        <div class="browse-slot">
-            <div class="browse-slot-image"></div>
-            <div class="browse-slot-text">Post 12</div>
-        </div>
+        @if(count($trendingPosts) > 0)
+            @foreach($trendingPosts as $post)
+                <div class="browse-slot">
+                    <div class="browse-slot-image"><img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image"></div>
+                    <div class="browse-slot-text">{{$post->title}}</div>
+                </div>
+            @endforeach
+        @endif
     </div>
 @endsection
