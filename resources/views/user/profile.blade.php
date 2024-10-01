5e793c607be8be13fc9546105216de6f149d8376
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-10-01 13:55:39 +0300

    Added follow/unfollow buttons to profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,11 +3,28 @@
 @section('content')
 <!-- Profile Section -->
 <section class="profile-section">
-    <img src="{{ asset($userInfo['profile_picture'] ? 'profile_pictures/' . $userInfo['profile_picture'] : 'assets/noimage.png') }}" alt="{{ $userInfo['name'] }}" class="profile-image">
+    <img src="{{ asset($user['profile_picture'] ? 'profile_pictures/' . $user['profile_picture'] : 'assets/noimage.png') }}" alt="{{ $user['name'] }}" class="profile-image">
     <div class="profile-info">
-        <h2>{{ $userInfo['name'] }}</h2>
+        <h2>{{ $user['name'] }}</h2>
         <div class="profile-stats">
             <div>{{ $followersCount }} followers</div>
+            @if (Auth::check())
+                @if (Auth::user()->isFollowing($user->id))
+                    <!-- Unfollow Form -->
+                    <form action="{{ route('unfollow', $user['id']) }}" method="POST" style="display:inline;">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Unfollow</button>
+                    </form>
+                @else
+                    <!-- Follow Form -->
+                    <form action="{{ route('follow', $user['id']) }}" method="POST" style="display:inline;">
+                        @csrf
+                        <button type="submit" class="btn btn-primary">Follow</button>
+                    </form>
+                @endif
+            @else
+                <p>Please <a href="{{ route('login') }}">login</a> to follow or unfollow users.</p>
+            @endif
         </div>
     </div>
 </section>
