6e232004ef4b91242451f25177fc9dcd0d1038d8
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-10-02 14:43:08 +0300

    Made posts clickable on homepage

--- resources/views/posts/homepage.blade.php
+++ resources/views/posts/homepage.blade.php
@@ -5,78 +5,76 @@
         <div class="homepage-block-title"><a href="/newest">Recent</a></div>
         <div class="homepage-content-slots">
             @if(count($newestPosts) > 0)
-                    @foreach($newestPosts as $post)
+                @foreach($newestPosts as $post)
                     <div class="homepage-slot">
-                        <div class="homepage-slot-image"><img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image"></div>
-                        <div class="homepage-slot-text">{{ $post->title }}</div>
+                        <a href="{{ route('posts.show', $post->id) }}" class="slot-link">
+                            <div class="homepage-slot-image">
+                                <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
+                            </div>
+                            <div class="homepage-slot-text">{{ $post->title }}</div>
+                        </a>
                     </div>
-                    @endforeach
+                @endforeach
             @else
                 <p>No posts found</p>
             @endif
         </div>
     </div>
+
     <div class="homepage-wide-container">
         <div class="homepage-block-title"><a href="/popular">Popular</a></div>
         <div class="homepage-content-slots">
             @if(count($popularPosts) > 0)
-                    @foreach($popularPosts as $post)
+                @foreach($popularPosts as $post)
                     <div class="homepage-slot">
-                        <div class="homepage-slot-image"><img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image"></div>
-                        <div class="homepage-slot-text">{{ $post->title }}</div>
+                        <a href="{{ route('posts.show', $post->id) }}" class="slot-link">
+                            <div class="homepage-slot-image">
+                                <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
+                            </div>
+                            <div class="homepage-slot-text">{{ $post->title }}</div>
+                        </a>
                     </div>
-                    @endforeach
+                @endforeach
             @else
                 <p>No posts found</p>
             @endif
         </div>
     </div>
+
     <div class="homepage-wide-container">
         <div class="homepage-block-title"><a href="/trending">Trending</a></div>
         <div class="homepage-content-slots">
             @if(count($trendingPosts) > 0)
-                    @foreach($trendingPosts as $post)
+                @foreach($trendingPosts as $post)
                     <div class="homepage-slot">
-                        <div class="homepage-slot-image"><img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image"></div>
-                        <div class="homepage-slot-text">{{ $post->title }}</div>
+                        <a href="{{ route('posts.show', $post->id) }}" class="slot-link">
+                            <div class="homepage-slot-image">
+                                <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
+                            </div>
+                            <div class="homepage-slot-text">{{ $post->title }}</div>
+                        </a>
                     </div>
-                    @endforeach
+                @endforeach
             @else
                 <p>No posts found</p>
             @endif
         </div>
     </div>
+
     @auth
     <div class="homepage-wide-container">
         <div class="homepage-block-title"><a href="/following">Following</a></div>
         <div class="homepage-content-slots">
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 1</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 2</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 3</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 4</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 5</div>
-            </div>
-            <div class="homepage-slot">
-                <div class="homepage-slot-image"></div>
-                <div class="homepage-slot-text">Slot 6</div>
-            </div>
+            @foreach (range(1, 6) as $i)
+                <div class="homepage-slot">
+                    <a href="#" class="slot-link"> <!-- Placeholder link for "following" posts -->
+                        <div class="homepage-slot-image"></div>
+                        <div class="homepage-slot-text">Slot {{ $i }}</div>
+                    </a>
+                </div>
+            @endforeach
         </div>
     </div>
     @endauth
 </div>
-
 @endsection
