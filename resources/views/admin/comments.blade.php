784867026bbb6d58e7c7199ba1e67489f0c6bfb1
21DP2DSmir <101799722+DanielSmirnov05@users.noreply.github.com>
2024-10-08 09:45:43 +0300

    Update comments.blade.php

--- resources/views/admin/comments.blade.php
+++ resources/views/admin/comments.blade.php
@@ -7,16 +7,16 @@
         <a href="{{ route('admin.comments') }}" class="adm-button">Comments</a>
         <a href="{{ route('admin.stats') }}" class="adm-button">Stats</a>
     </div>
-    <h1 style="margin-left: auto; margin-right: auto; text-align: center;">Post List</h1>
+    <h1 style="margin-left: auto; margin-right: auto; text-align: center;">Comment List</h1>
     <div class="table-container">
         <table class="table">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>User ID</th>
-                    <th>Title</th>
-                    <th>Description</th>
+                    <th>Body</th>
                     <th>Created At</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -27,6 +27,13 @@
                         <td>{{ $comment->post_id }}</td>
                         <td>{{ $comment->body }}</td>
                         <td>{{ $comment->created_at->format('Y-m-d H:i') }}</td>
+                        <td>
+                            <form action="{{ route('admin.comments.delete', $comment->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this comment?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
@@ -34,3 +41,4 @@
     </div>
 </div>
 @endsection
+
