bad15ec2f675b5064ddea39651b4859cc5a3a98e
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-10-14 17:16:10 +0300

    Update homepage.blade.php

--- resources/views/posts/homepage.blade.php
+++ resources/views/posts/homepage.blade.php
@@ -65,16 +65,23 @@
     <div class="homepage-wide-container">
         <div class="homepage-block-title"><a href="/following">Following</a></div>
         <div class="homepage-content-slots">
-            @foreach (range(1, 6) as $i)
-                <div class="homepage-slot">
-                    <a href="#" class="slot-link"> <!-- Placeholder link for "following" posts -->
-                        <div class="homepage-slot-image"></div>
-                        <div class="homepage-slot-text">Slot {{ $i }}</div>
-                    </a>
-                </div>
-            @endforeach
+            @if(count($followingPosts) > 0)
+                @foreach($followingPosts as $post)
+                    <div class="homepage-slot">
+                        <a href="{{ route('posts.show', $post->id) }}" class="slot-link">
+                            <div class="homepage-slot-image">
+                                <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
+                            </div>
+                            <div class="homepage-slot-text">{{ $post->title }}</div>
+                        </a>
+                    </div>
+                @endforeach
+            @else
+                <p>No posts found</p>
+            @endif
         </div>
     </div>
     @endauth
 </div>
 @endsection
+
