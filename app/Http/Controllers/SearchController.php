e2deeabec4e5695dddbecb2dad319f6efeb59895
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2024-10-16 12:25:54 +0300

    Update SearchController.php
    attempt 1 search controller done

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
     public function filter(Request $request)
     {
-        // Kodu raksti šeit
+        // Validēt atslēgvārdus
+        $request->validate([
+            'keywords' => 'required|string',
+        ]);
+
+        $keywords = $request->input('keywords');
+
+        // Meklēt publikācijas, kur atslēgvārdi ir līdzīgi nosaukumam, aprakstam vai autora vārdam
+        $posts = Post::where('title', 'like', '%' . $keywords . '%')
+            ->orWhere('description', 'like', '%' . $keywords . '%')
+            ->orWhereHas('author', function ($query) use ($keywords) {
+                $query->where('name', 'like', '%' . $keywords . '%');
+            })
+            ->get();
+
+        // Atgriezt atrastās publikācijas
+        return response()->json($posts);
     }
 }
