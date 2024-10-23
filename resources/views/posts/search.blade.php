d8cbb812050116343a129ded94c15a3bb5553c19
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-10-23 12:17:17 +0300

    Update search.blade.php

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
     <h1 style="margin-left: auto; margin-right: auto; text-align: center">Search Results</h1>
-    @if($results->isEmpty())
+    @if($posts->isEmpty())
         <p>No results found for "{{ $query }}".</p>
     @else
-    <div class="grid-container">
-        @foreach($results as $post)
-            <div class="post-preview">
-                <a href="{{ route('posts.show', ['post' => $post->id]) }}" class="post-link">
-                    <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
-                    <div class="post-info">
-                        <div class="post-user">{{ $post->user->name }}</div>
-                        <div class="post-title">{{ $post->title }}</div>
+    <div class="browse-content-slots">
+        @if(count($posts) > 0)
+            @foreach($posts as $post)
+                <div class="browse-slot">
+                    <a href="{{ route('posts.show', $post->id) }}" class="slot-link">
+                        <div class="browse-slot-image">
+                            <img src="{{ asset($post->cover_image ? 'cover_images/' . $post->cover_image : 'assets/noimage.png') }}" alt="{{ $post->title }}" class="post-image">
+                        </div>
+                        <div class="browse-slot-text">{{ $post->title }}</div>
+                    </a>
                 </div>
-                </a>
-            </div>
-        @endforeach
+            @endforeach
+        @endif
     </div>
     @endif
 @endsection
