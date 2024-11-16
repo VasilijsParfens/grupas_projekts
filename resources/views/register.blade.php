ca877973ed14486339cbd55a70139d8996bdffe0
21DP2VParf <106649073+VasilijsParfens@users.noreply.github.com>
2024-11-16 13:57:39 +0200

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,15 +1,6 @@
 @extends('layout')
-@section('content')
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
+@section('content')
     <div class="register-container">
         <form action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
             @csrf
@@ -17,23 +8,43 @@
                 <div class="reg-input">
                     <label for="name">Name</label>
                     <input placeholder="Name:" type="text" id="name" name="name" value="{{ old('name') }}" required>
+                    @error('name')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="reg-input">
                     <label for="email">E-mail</label>
                     <input placeholder="E-mail:" type="email" id="email" name="email" value="{{ old('email') }}" required>
+                    @error('email')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="reg-input">
                     <label for="password">Password</label>
                     <input placeholder="Password:" type="password" id="password" name="password" required>
+                    @error('password')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="reg-input">
-                    <label for="password_confirm">Confirm password</label>
+                    <label for="password_confirmation">Confirm password</label>
                     <input placeholder="Confirm password:" type="password" id="password_confirmation" name="password_confirmation" required>
+                    @error('password_confirmation')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="reg-input">
                     <label for="profile_picture">Upload picture:</label>
                     <input placeholder="Upload picture" type="file" name="profile_picture" accept="image/*">
+                    @error('profile_picture')
+                        <div class="error">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="reg-input">
                     <button type="submit">Register</button>
                 </div>
